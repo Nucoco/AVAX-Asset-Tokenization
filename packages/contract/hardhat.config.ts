c55b033aec9dccd3d683840d771862f393873d8a
Nucoco <yuuta11511997@gmail.com>
2024-04-20 07:44:33 +0000

    コントラクトデプロイと型定義ファイルのフロント側へのコピー

--- packages/contract/hardhat.config.ts
+++ packages/contract/hardhat.config.ts
@@ -1,8 +1,26 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+import * as dotenv from 'dotenv';
+import '@nomicfoundation/hardhat-toolbox';
+import { HardhatUserConfig } from 'hardhat/config';
+
+// .envファイルから環境変数をロード
+dotenv.config();
+
+if (process.env.TEST_ACCOUNT_PRIVATE_KEY === undefined) {
+  console.log('private key is missing');
+}
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.24",
+  solidity: '0.8.24',
+  networks: {
+    fuji: {
+      url: 'https://api.avax-test.network/ext/bc/C/rpc',
+      chainId: 43113,
+      accounts:
+        process.env.TEST_ACCOUNT_PRIVATE_KEY !== undefined
+          ? [process.env.TEST_ACCOUNT_PRIVATE_KEY]
+          : [],
+    },
+  },
 };
 
 export default config;
